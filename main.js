8b68d0559f900a472047ca8c46e97c7d5348c4ac
sanidhya0612 <102963033+sanidhya0612@users.noreply.github.com>
2023-09-02 16:38:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var songa = "";
 var songb = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 
 function preload()
 {
@@ -14,7 +19,35 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+    console.log("posenet is initialized!!")
+
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " +leftWristY);
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " +rightWristX + " rightWristY = " + rightWristY);
+    }
+}
+
 
 function draw()
 {
